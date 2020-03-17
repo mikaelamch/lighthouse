cf2b287dd336a1265e3c1e43f83da864a0b39dff
Anagha <anagha.reghunath.2571@student.uu.se>
2020-03-17 18:16:36 +0100

    Removal of unused files and some code refactoring

---
+++ Scripts/BartenderTables.js
@@ -0,0 +1,240 @@
+/*All data models are defined in BartenderViewModel.js which can be modified for any changes required in View.*/
+
+/*Initialize ordersMatrix for keeping track of state of orders
+ordersMatrix is a 2D matrix with its values assigned as arrays of length 3. For eg., ordersMatrix[x][y] = [1,1,0] where x gives us the table number and y gives us the order number
+the indices from 0 to 2 in arrays of length 3 represent 'order placed status', 'order delivered status' and 'order paid for status' respectively.
+For eg., ordersMatrix[2][5]=[1,1,0] indicates that Table 3 order 6 has been placed(1) and delivered(1) but hasn't been paid for(0).*/
+if(!localStorage.getItem('ordersMatrix')) {//app being launched for the first time
+    var ordersMatrix = [];
+    let rows = 6;
+    let columns = 6;
+    for (let i = 0; i < rows; i++) {
+        ordersMatrix.push([0]);
+        for (var j = 0; j < columns; j++) {
+            ordersMatrix[i][j] = [0, 0, 0];
+        }
+    }
+    localStorageSetObj('ordersMatrix', ordersMatrix);
+}
+else
+    ordersMatrix = localStorageGetObj('ordersMatrix'); //get ordersMatrix from local storage if it exists already
+
+
+/*set and get functions from local browser storage*/
+function localStorageSetObj(key, obj) {
+    localStorage.setItem(key, JSON.stringify(obj))
+}
+function localStorageGetObj(key) {
+    return JSON.parse(localStorage.getItem(key))
+}
+
+/*Orders 1-6 to be added to view when order is placed*/
+var ordersArray= [
+    dataViewModels.ordersArray_1,
+    dataViewModels.ordersArray_2,
+    dataViewModels.ordersArray_3,
+    dataViewModels.ordersArray_4,
+    dataViewModels.ordersArray_5,
+    dataViewModels.ordersArray_6
+];
+
+
+function viewOrderItems(element, orderNumber){
+    //remove previous list added
+    let tabContents = document.getElementById("Table_Inner_Container");
+    if(tabContents.lastChild.id !== 'Tables_container_top')
+    tabContents.removeChild(tabContents.lastChild);
+    //Get Table name
+    let tableName = element.parentElement.id;
+    //creatig dymanic ids for the checkboxes
+    let deliveredCBId = 'del-'+tableName+'-'+ orderNumber;
+    let paidCBId = 'paid-'+tableName+'-'+ orderNumber;
+
+    //check for delivery status before displaying
+    let tableNum = tableName.split("_")[1];
+    let deliveredStatusNum = localStorageGetObj('ordersMatrix')[tableNum-1][orderNumber-1][1]; //get the delivery status of the viewed order from ordersMatrix
+    let deliveredStatus = '';
+    if(deliveredStatusNum)
+        deliveredStatus = 'checked'; //restore the delivered status when viewing previous orders that has been delivered
+    let disablepaidBox = !deliveredStatusNum ?'disabled="disabled"':''; //paid box should be disabled only if delivered box is not checked
+
+    //Add a list with order items on screen
+    let ordersList = '<div class="orders_list"><br><b>Table :</b>' +tableName+'<br><b>Order Number :</b>' + orderNumber+
+        '<br><br>Pina colada' + //need to remove hardcoded data and populate with actual data
+        '<br>Bloody Mary'+
+        '<br>Cheese Fries (2)'+
+        '<br>Samuel Adams Winter Lager<br><br>'+
+        '<input type="checkbox" id='+deliveredCBId+' class="delivered_cb" name="delivered" value="delivered" onclick=\'handleDeliveredClick(this)\' '+deliveredStatus+'>: Delivered<br>'+
+        '<input type="checkbox" id='+paidCBId+' class="paid_cb" name="paid" value="paid" onclick=\'handlePaidClick(this)\' '+ disablepaidBox +'>: Paid' +
+        '</div>';
+    tabContents.insertAdjacentHTML('beforeend', ordersList);
+}
+
+function handlePaidClick(cb) {
+    if(cb.checked){ //if paid checkbox has been checked
+        let cbParents = cb.id.split("-");
+        let tableName = cbParents[1];
+        let orderNum = cbParents[2];
+
+        //get parent element with table name id
+        let table = document.getElementById(tableName);
+        //find its child with the order number and remove.
+        let childOrder = document.querySelectorAll('#'+tableName+' .order_'+orderNum)[0];
+        //remove
+        table.removeChild(childOrder);
+        let tableNum = table.id.split("_")[1];
+        //save state of being delivered & paid
+        ordersMatrix[tableNum-1][orderNum-1]=[1,1,1];
+        localStorageSetObj('ordersMatrix',ordersMatrix);
+        //close items view when paid option is checked
+        let tabContents = document.getElementById("Table_Inner_Container");
+        tabContents.removeChild(tabContents.lastChild);
+    }
+}
+
+function handleDeliveredClick(cb) { //called when delivered checkbox is clicked
+    let cbParents = cb.id.split("-"); //get the checkbox parent table and order number from its id
+    let tableNum = cbParents[1].split("_")[1];
+    let orderNum = cbParents[2];
+    // because we need to save state of order if order is delivered but not paid for
+    ordersMatrix[tableNum-1][orderNum-1]=[1,1,0];
+    localStorageSetObj('ordersMatrix',ordersMatrix);
+    //construct paidbox id from deliveredbox id & enable paid checkbox
+    let paidCBId = 'paid-'+cbParents[1]+'-'+ orderNum;
+    document.getElementById(paidCBId).disabled = false;
+}
+
+function handleDisableClick(cb){ //adds disabled css effects to items
+    let itemId = cb.id.split('_')[0];
+    let slNoField =  document.getElementById(itemId + '_item_sl');
+    let itemName = document.getElementById(itemId + '_item_name');
+    let stockQty = document.getElementById(itemId + '_sqty');
+
+    if(cb.checked){
+        //add disable class
+
+        slNoField.classList.add("disable");
+        itemName.classList.add("disable");
+        stockQty.classList.add("disable");
+    } else{
+        //remove disable class
+        slNoField.classList.remove("disable");
+        itemName.classList.remove("disable");
+        stockQty.classList.remove("disable");
+    }
+
+}
+
+//utility method to compare 2 arrays, can be moved out of file if required
+function isEqual(array1,array2)
+{        // comparing each element of array
+        for(let i=0;i<array1.length;i++) {
+            if (array1[i] != array2[i]) {
+                return false;
+            }
+        }
+        return true;
+}
+
+$(function() { //executes only after the final page with all components are loaded
+
+    //Author:Juhee
+    //Add click events to tabs
+    var tableTab = document.getElementById("bartender_table_view_txt");
+    var stockTab = document.getElementById("bartender_stock_view_txt");
+    var houseTab = document.getElementById("bartender_house_view_txt");
+    tableTab.addEventListener("click", openPage.bind(this,'Table', tableTab, '#462a17'));
+    stockTab.addEventListener("click", openPage.bind(this,'Stock', stockTab, '#462a17'));
+    houseTab.addEventListener("click", openPage.bind(this,'House', houseTab, '#462a17'));
+    tableTab.click(); //loads the table view by default
+
+    function openPage(pageName, elmnt, color) {
+        // Hide all elements with class="tabcontent" by default */
+        let i, tabcontent, tablinks;
+        tabcontent = document.getElementsByClassName("tabcontent");
+        for (let i = 0; i < tabcontent.length; i++) {
+            tabcontent[i].style.display = "none";
+        }
+        // Remove the background color of all tablinks/buttons
+        tablinks = document.getElementsByClassName("tablink");
+        for (let i = 0; i < tablinks.length; i++) {
+            tablinks[i].style.backgroundColor = "";
+        }
+        // Show the specific tab content
+        document.getElementById(pageName).style.display = "block";
+        // Add the specific color to the button used to open the tab content
+        elmnt.style.backgroundColor = color;
+    }
+
+    //time bar at the bottom
+    var currentTime = new Date(),
+        year = currentTime.getFullYear(),
+        day = currentTime.getDate(),
+        minutes = currentTime.getMinutes();
+    if (minutes < 10) {
+        minutes = "0" + minutes;
+    }
+    if (day < 10)  {
+        day = "0" + day;
+    }
+    const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
+        "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"];
+    const dayNames = ["Mon", "Tue", "Wed", "Thur", "Fri", "Sat", "Sun"];
+    //document.getElementById('time').innerHTML = hours + ":" + minutes;
+    document.getElementById('today').innerHTML = year + " " + monthNames[currentTime.getMonth()] +  " " + day +  " " + dayNames[currentTime.getDay()];
+
+    //Author:Anagha
+    /* to be removed later- dummy button functionality*/
+    var orderPlaceButtons = document.getElementsByClassName("place_order_btn");
+    orderPlaceButtons = Array.from(orderPlaceButtons);
+    orderPlaceButtons.forEach(function(btn){
+        let tableNum = Number(btn.innerText.charAt(btn.innerText.length-1)-1);
+        btn.addEventListener("click", displayOrder.bind(this,btn.innerText,tableNum));
+    });
+    /*to be removed later*/
+
+    if(localStorage.getItem('ordersMatrix')){//if ordersMatrix exists in localStorage, load current orders from it
+        //populate the tables with orders
+        for(let tNum=0;tNum < 6; tNum++) {//tables
+            for(let oNum=0;oNum<6;oNum++){//orders
+                if(ordersMatrix[tNum][oNum][0] == 1 && ordersMatrix[tNum][oNum][2] != 1) { //'order placed status' is 1 and 'order paid status' is not 1
+                    let tableNum = tNum+1;
+                    displayOrderOnLoad('Table_' + tableNum, tNum);
+                    break;
+                }
+            }
+        }
+    }
+
+    //Append order to view
+    function displayOrder(tableName, tableNumber) { //to add orders to view when customer places order
+
+        for (let j = 0; j < 6; j++) {
+            //if an order has been paid & cleared OR if the order was never placed
+            if (isEqual(localStorageGetObj('ordersMatrix')[tableNumber][j], [1, 1, 1]) || isEqual(localStorageGetObj('ordersMatrix')[tableNumber][j], [0, 0, 0])) {
+               //paid & cleared order vacancies at each table can now be treated as new orders
+                let order = ordersArray[j]; //insert order from the ordersArray based on order vacancies
+                let table = document.getElementById(tableName);
+                table.insertAdjacentHTML('beforeend', order);
+                ordersMatrix[tableNumber][j]=[1,0,0]; //save state in the ordersMatrix (order placed, not delivered, not paid)
+                localStorageSetObj('ordersMatrix',ordersMatrix);
+                break;
+            }
+        }
+
+    }
+    function displayOrderOnLoad(tableName, tableNumber) { //to reload all the current orders from local storage when app is refreshed
+
+        for (let j = 0; j < 6; j++) {
+            //paid or unplaced orders need not be displayed
+            if (!isEqual(localStorageGetObj('ordersMatrix')[tableNumber][j], [1, 1, 1]) && !isEqual(localStorageGetObj('ordersMatrix')[tableNumber][j], [0, 0, 0])) {
+                let order = ordersArray[j]; //insert order from the ordersArray
+                let table = document.getElementById(tableName);
+                table.insertAdjacentHTML('beforeend', order);
+                localStorageSetObj('ordersMatrix',ordersMatrix);
+
+            }
+        }
+    }
+
+});
---
+++ Scripts/BartenderViewModel.js
@@ -0,0 +1,41 @@
+var dataViewModels = {};
+
+
+/*Data Models used in Bartender.js file*/
+dataViewModels.ordersArray_1 = '<div class="table_orders order_1" onClick="viewOrderItems(this, 1)">1</div>';
+dataViewModels.ordersArray_2 = '<div class="table_orders order_2" onClick="viewOrderItems(this, 2)">2</div>';
+dataViewModels.ordersArray_3 = '<div class="table_orders order_3" onClick="viewOrderItems(this, 3)">3</div>';
+dataViewModels.ordersArray_4 = '<div class="table_orders order_4" onClick="viewOrderItems(this, 4)">4</div>';
+dataViewModels.ordersArray_5 = '<div class="table_orders order_5" onClick="viewOrderItems(this, 5)">5</div>';
+dataViewModels.ordersArray_6 = '<div class="table_orders order_6" onClick="viewOrderItems(this, 6)">6</div>';
+
+
+
+/*Data Models used in Bartender2.js file*/
+dataViewModels.refreshHtml_1 = '<div class=\"saved_items_cls\"></div>';
+dataViewModels.refreshHtml_2a = '<div class=\"saved_qty_cls\"> (';
+dataViewModels.refreshHtml_2b = ')</div>';
+
+dataViewModels.stock_1a = '<div id=';
+dataViewModels.stockSlNo_1b = '_item_sl class="flexbox4_sl_no">' ;
+dataViewModels.stockSlNo_1c = '. </div>';
+dataViewModels.stock_1c = '</div>';
+
+dataViewModels.stockItemsColumn_1b = '_item_name class="flexbox4_items_column1">' ;
+dataViewModels.stockNumberColumn_1b = '_sqty class="flexbox4_items_column2">' ;
+
+dataViewModels.stockDisableColumn_1a = '<div class="flexbox4_items_column3">';
+dataViewModels.stockDisableColumn_1b = '<input type="checkbox" id=';
+dataViewModels.stockDisableColumn_1c = '_item_cb onclick=\'handleDisableClick(this)\'> </div>';
+
+dataViewModels.houseStockSlNo = '<div class="flexbox4_sl_no">';
+dataViewModels.houseStockItemsColumn = '<div class="flexbox4_items_column1">';
+
+dataViewModels.savedItems_1a = '<div class="saved_items_cls">';
+dataViewModels.savedItems_1b = '<div class="saved_qty_cls"> (';
+dataViewModels.savedItems_1c = ')</div>';
+
+
+
+
+
