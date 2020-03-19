84b18cf3fb2e59bf530b0dcf2e6219221dd9ffa7
Anagha <anagha.reghunath.2571@student.uu.se>
2020-03-19 12:54:28 +0100

    Language translation capability added for dynamic components added to view

--- Scripts/TranslationControl.js
+++ Scripts/TranslationControl.js
@@ -1,20 +1,28 @@
-//Author: Anagha
-$(function() {
-    //read the text from translation.json file to populate the view
-    function populateText() {
-                $.getJSON("JSON/Translation.json", function (translationJson) {
-                    //Get the selected language
-                    var langSelection = document.getElementsByClassName("lang")[0];
-                    var language = langSelection.options[langSelection.selectedIndex].value;
 
-                    //function to populate text based on language selection
-                    var allTxtIDs = $("[id*='txt']"); //all components in view with that contains txt in their ID
 
-                    allTxtIDs.each(function (index, item) {
-                        item.innerHTML = translationJson[language][item.id];
-                    });
+/*This is the function to populate text based on language selection.
+* Read the text from translation.json file to populate the view*/
+
+function populateText(component) {
+    $.getJSON("JSON/Translation.json", function (translationJson) {
+        //Get the selected language
+        let langSelection = document.getElementsByClassName("lang")[0];
+        let language = langSelection.options[langSelection.selectedIndex].value;
+        let allTxtIDs;
+
+        if(component){ //translate only for a sub-component of the view (for dynamically added components)
+            allTxtIDs = $(component + " *[id*='txt']");
+        }
+        else
+            allTxtIDs = $("[id*='txt']"); //all components in view with that contains txt in their ID
+
+        allTxtIDs.each(function (index, item) {
+            item.innerHTML = translationJson[language][item.id];
         });
-    }
+     });
+}
+
+$(function() {
     //change language selection event
     $( ".lang" ).change(function() {
         populateText();
